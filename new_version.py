7e206e8c25e2aece1146a74f703007053c788449
冰梦之音 <68908911+bingmeng158@users.noreply.github.com>
2023-06-27 20:44:18 +0800

    Update new_version.py

--- new_version.py
+++ new_version.py
@@ -1,12 +1,20 @@
-# new_version.py
+import json
 
-import json;
-import time;
+# 读取 package.json 文件
+with open('package.json', 'r') as file:
+    data = json.load(file)
 
-with open("package.json",'r',encoding='utf-8') as f:
-    jspack = json.load(f)
+# 获取当前版本号
+current_version = data['version']
 
-new_version = '1.1.'+str(int(time.time()))[0]
-jspack['version']=new_version
-with open("package.json",'w',encoding='utf-8') as f:
-    json.dump(jspack, f,ensure_ascii=False)
+# 递增版本号
+major, minor, patch = map(int, current_version.split('.'))
+patch += 1
+new_version = f"{major}.{minor}.{patch}"
+
+# 更新版本号
+data['version'] = new_version
+
+# 写入更新后的 package.json 文件
+with open('package.json', 'w') as file:
+    json.dump(data, file, indent=4)
