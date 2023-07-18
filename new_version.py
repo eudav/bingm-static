4c42366672024e77e0d98ae082e975885dd2e13d
冰梦 <116251751+eudav@users.noreply.github.com>
2023-07-19 00:03:55 +0800

    Update new_version.py

--- new_version.py
+++ new_version.py
@@ -1,11 +1,20 @@
 import json;
-import time;
 
-with open("package.json", 'r', encoding='utf-8') as f:
+# 读取 package.json 文件
+with open('package.json', 'r',encoding='utf-8') as f:
     jspack = json.load(f)
 
-new_version = '1.1.' + str(int(time.time()))[1:11:1]
-hort_version = new_version[:5]  # 截取前 5 个字符
-jspack['version'] = hort_version
-with open("package.json", 'w', encoding='utf-8') as f:
-    json.dump(jspack, f, ensure_ascii=False)
+# 获取当前版本号
+current_version = jspack['version']
+
+# 递增版本号
+major, minor, patch = map(int, current_version.split('.'))
+patch += 1
+new_version = f"{major}.{minor}.{patch}"
+
+# 更新版本号
+jspack['version'] = new_version
+
+# 写入更新后的 package.json 文件
+with open('package.json', 'w',encoding='utf-8') as f:
+    json.dump(jspack, f,ensure_ascii=False)
